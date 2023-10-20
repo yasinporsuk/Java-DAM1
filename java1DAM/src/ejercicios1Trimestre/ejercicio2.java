f0d43c7911dca74758227d7a7432c81957f8febe
yasinporsuk <yasinporsuk@gmail.com>
2023-10-20 11:11:58 +0200

    ejercicio2

--- java1DAM/src/ejercicios1Trimestre/ejercicio2.java
+++ java1DAM/src/ejercicios1Trimestre/ejercicio2.java
@@ -1,8 +1,23 @@
 package ejercicios1Trimestre;
 
+import java.util.Scanner;
+
 public class ejercicio2 {
 
 	public static void main(String[] args) {
+		
+		Scanner tcl = new Scanner(System.in);
+		System.out.println("Introduzca nuemro para saber si es mayor o menor de 0");
+		int numero = tcl.nextInt();
+		
+		if (numero > 0) { 
+			System.out.println("El numero introducido es mayor que 0");
+		
+		} else { 
+			System.out.println("El numero introducido es menor que 0");
+		}
+		
+		
 		
 		
 	}
